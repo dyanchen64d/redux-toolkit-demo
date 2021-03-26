aed98222806926c26bca0af6acc6ca9aeeac1aa4
chen.zhang <zhangchen64d@gmail.com>
2021-03-26 15:21:12 +0900

    middleware test

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,31 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { Middleware } from 'redux';
 import counterReducer from '../features/counter/counterSlice';
+
+const middlewares: any[] = [];
+
+const logIsMiddlemare: Middleware = (storeAPI) => next => action => {
+  console.log('middleware action', action);
+  console.log('storeAPI', storeAPI)
+  return next(action);
+}
+
+middlewares.push(logIsMiddlemare);
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: counterReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: {
+        payload: {
+          payload1: 'payload1',
+          payload2: 'payload2'
+        }
+      }
+    }
+  }).concat(middlewares)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
